ba52e8d7259654573969ab4a4e2b4b31bee8d325
Timo <developer.timo@gmail.com>
2021-12-16 00:07:16 +0900

    [스프링 타입 컨버터] 타입 컨버터 - Converter > IpPort 문자열 컨버터 테스트 검증

--- src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.converter;
 
+import hello.typeconverter.type.IpPort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,22 @@ public class ConverterTest {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String result = converter.convert(30);
         Assertions.assertThat(result).isEqualTo("30");
+    }
+
+    @Test
+    void ipPortToString() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        Assertions.assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void stringToIpPort() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        Assertions.assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
     }
 
 }
