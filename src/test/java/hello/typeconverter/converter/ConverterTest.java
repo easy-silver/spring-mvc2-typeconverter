e14f54d47cc7f311687a99b1ad4937f31d4541d6
Timo <developer.timo@gmail.com>
2021-12-15 23:50:02 +0900

    [스프링 타입 컨버터] 타입 컨버터 - Converter > 정수,문자 변환 컨버터 테스트 검증

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,22 @@
+package hello.typeconverter.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        Assertions.assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void integerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(30);
+        Assertions.assertThat(result).isEqualTo("30");
+    }
+
+}
